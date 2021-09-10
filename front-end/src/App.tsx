93c246d8c89b26c3464e7af6896fc34058c77105
Preston Davis <prem.dav@outlook.com>
2021-09-09 21:17:37 -0500

    placeholder landing page restyled and will work for the time being

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,11 +4,12 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>The Abide Project</h1>
-        <h5><h3 style={{ display: 'inline-block'}}><em>Abide:</em></h3> to remain stable or fixed in state</h5>
-        <p>"Abide in me, and I in you. As the branch cannot bear fruit by itself, unless it abides in the vine, neither can you, unless you abide in me."</p>
+        <h1>The <span className='abide'>Abide</span> Project</h1>
+        <p>"<span className='abide'>Abide</span> in me, and I in you. As the branch cannot bear fruit by itself, unless it <span className='abide'>abides</span> in the vine, neither can you, unless you <span className='abide'>abide</span> in me."</p>
         <p><em>John 15:4, ESV</em></p>
+        <h5><h3 style={{ display: 'inline-block'}}><em className='abide'>Abide:</em></h3> to remain stable or fixed in state</h5>
       </header>
+      <footer>Bible reading tracker currently in development. Come back often for updates!</footer>
     </div>
   );
 }
