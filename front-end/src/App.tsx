ad70b68201d0084ce989a0df7e3c8193ed1529fe
Preston Davis <prem.dav@outlook.com>
2021-09-09 21:02:20 -0500

    Placeholder text until a landing page gets built out

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,20 +1,13 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The Abide Project</h1>
+        <h5><h3 style={{ display: 'inline-block'}}><em>Abide:</em></h3> to remain stable or fixed in state</h5>
+        <p>"Abide in me, and I in you. As the branch cannot bear fruit by itself, unless it abides in the vine, neither can you, unless you abide in me."</p>
+        <p><em>John 15:4, ESV</em></p>
       </header>
     </div>
   );
