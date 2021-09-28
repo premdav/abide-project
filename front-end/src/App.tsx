e1ee776040bedc629fda15da5784ddcb5034fe09
Preston Davis <prem.dav@outlook.com>
2021-09-28 15:41:45 -0500

    Changed styling a little and changes footer on main placeholder page.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,11 +5,13 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>The <span className='abide'>Abide</span> Project</h1>
+        <p className='definition'><span style={{ display: 'inline-block', fontWeight: 'bold'}}><em className='abide'>Abide:</em></span> to remain stable or fixed in state</p>
         <p>"<span className='abide'>Abide</span> in me, and I in you. As the branch cannot bear fruit by itself, unless it <span className='abide'>abides</span> in the vine, neither can you, unless you <span className='abide'>abide</span> in me."</p>
         <p><em>John 15:4, ESV</em></p>
-        <h5><h3 style={{ display: 'inline-block'}}><em className='abide'>Abide:</em></h3> to remain stable or fixed in state</h5>
       </header>
-      <footer>Bible reading tracker currently in development. Come back often for updates!</footer>
+      <footer>
+        The Abide Project is a Bible reading tracker with a goal of getting more people engaged with the word of God. Please visit back for updates!
+      </footer>
     </div>
   );
 }
