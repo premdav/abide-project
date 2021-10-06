cda28d23ccf6205aee150d42fba745bebaeb91e3
Preston Davis <35507470+premdav@users.noreply.github.com>
2021-10-06 15:20:33 -0500

    Adds start scripts and proxy for api requests on front end (#13)
    * adds start and shutdown script to project
    
    * Sets proxy for api routes to be redirected to 8000

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './Stylesheets/Main.sass'
 
 ReactDOM.render(
